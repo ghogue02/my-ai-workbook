97d05e9b776f7128fa0bd9370c695321c3f89736
Greg Hogue <greg.hogue@gmail.com>
2025-01-09 14:16:44 -0500

    Fix planning page default export

---
+++ app/(protected)/planning/page.tsx
@@ -0,0 +1,11 @@
+// app/(protected)/planning/page.tsx
+"use client";
+
+export default function PlanningPage() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1 className="text-2xl">Planning</h1>
+      <p>This is a protected planning route!</p>
+    </div>
+  );
+}
